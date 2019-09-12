0af6d5df3edccfe9219a7ddd284920937cad153c
Your Name <you@example.com>
2019-09-12 22:54:52 +0800

    Initial Commit

---
+++ IRPlayerUIShell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IRPlayerUIShell
+//
+//  Created by irons on 2019/9/12.
+//  Copyright © 2019年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IRPlayerUIShell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IRPlayerUIShell
+//
+//  Created by irons on 2019/9/12.
+//  Copyright © 2019年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
